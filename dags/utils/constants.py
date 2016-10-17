1c2c99c264c5f007c78ba163f801c4bd37d93b06
Vitor Baptista <vitor@vitorbaptista.com>
2016-10-17 13:06:56 +0200

    WIP

--- dags/utils/constants.py
+++ dags/utils/constants.py
@@ -1,7 +1,1 @@
-DS_WEEKLY = (
-    '{% if dag_run.external_trigger %}'
-        '{{ ds_nodash }}'
-    '{% else %}'
-        '{{ macros.ds_format(macros.ds_add(ds, 7), "%Y-%m-%d", "%Y%m%d") }}'
-    '{% endif %}'
-)
+PROCESSOR_IMAGE = 'okibot/processors:latest'
