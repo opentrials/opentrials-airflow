1f10a65bf5b4ab2d54750381bca767d101d8ba2e
Vitor Baptista <vitor@vitorbaptista.com>
2017-03-02 16:59:04 +0000

    [#628] Run the data_contributions processor daily, instead of using a sensor
    opentrials/opentrials#628

--- dags/data_contributions.py
+++ dags/data_contributions.py
@@ -1,5 +1,4 @@
 import datetime
-import airflow.operators
 from airflow.models import DAG
 import utils.helpers as helpers
 
@@ -18,16 +17,7 @@ dag = DAG(
     schedule_interval='@daily'
 )
 
-unprocessed_data_contributions_sensor_task = airflow.operators.SqlSensor(
-    task_id='unprocessed_data_contributions_sensor',
-    dag=dag,
-    conn_id='explorer_db',
-    sql='SELECT id FROM data_contributions WHERE approved=true AND document_id IS NULL'
-)
-
 data_contributions_processor_task = helpers.create_processor_task(
     name='data_contributions',
     dag=dag
 )
-
-data_contributions_processor_task.set_upstream(unprocessed_data_contributions_sensor_task)
