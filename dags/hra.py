d515230a0dd4a39a8109d8d4590201dc70deec58
Vitor Baptista <vitor@vitorbaptista.com>
2017-03-16 20:37:08 +0000

    [#698] Limit HRA collection times
    opentrials/opentrials#698

--- dags/hra.py
+++ dags/hra.py
@@ -1,11 +1,12 @@
-from datetime import datetime
+import datetime
 import airflow.models
 import utils.helpers as helpers
+from operators.python_sensor import PythonSensor
 
 args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2016, 12, 1),
+    'start_date': datetime.datetime(2016, 12, 1),
     'retries': 1,
 }
 
@@ -14,6 +15,24 @@ dag = airflow.models.DAG(
     default_args=args,
     max_active_runs=1,
     schedule_interval='@monthly'
+)
+
+
+def _check_hra_api_availability():
+    now = datetime.datetime.now()
+
+    is_mon_wed_or_thrusday = (now.weekday() in [0, 2, 3])
+    is_after_6am = (now.time() >= datetime.time(6, 0))
+    is_before_8am = (now.time() < datetime.time(8, 0))
+    is_between_6am_and_8am = (is_after_6am and is_before_8am)
+
+    return is_mon_wed_or_thrusday and is_between_6am_and_8am
+
+
+wait_for_hra_api_availability_sensor = PythonSensor(
+    task_id='wait_for_hra_api_availability',
+    dag=dag,
+    python_callable=_check_hra_api_availability
 )
 
 collector_task = helpers.create_collector_task(
@@ -37,5 +56,6 @@ hra_linker_task = helpers.create_processor_task(
     dag=dag
 )
 
+collector_task.set_upstream(wait_for_hra_api_availability_sensor)
 processor_task.set_upstream(collector_task)
 hra_linker_task.set_upstream(processor_task)
